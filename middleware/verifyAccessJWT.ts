8924541f3fdb4e66237e701263d9bae36cae07b0
Luigi <luigicode2@gmail.com>
2023-10-21 17:40:22 +0200

    add auth messages

--- middleware/verifyAccessJWT.ts
+++ middleware/verifyAccessJWT.ts
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 export const verifyAccessJWT = (req: any, res: any, next: any) => {
   // Get headers
   const authHeader = req.headers.authorization;
-  if (!authHeader?.startsWith("Bearer")) return res.sendStatus(401);
+  if (!authHeader?.startsWith("Bearer")) return res.status(401).json({ message: "No bearer" });
   // Separete bearer
   const bearer = authHeader.split(" ")[1];
   jwt.verify(bearer, process.env.ACCESS_TOKEN_SECRET, (err: any, decoded: any) => {
--- middleware/verifyRoles.ts
+++ middleware/verifyRoles.ts
@@ -2,9 +2,9 @@ import { Request, Response, NextFunction } from "express";
 
 const verifyRoles = (rolesArray: any[]) => {
   return (req: any, res: Response, next: NextFunction) => {
-    if (!req?.role) return res.sendStatus(401);
+    if (!req?.role) return res.status(401).json({ message: "No roles provided" });
     const result = rolesArray.includes(req.role);
-    if (!result) return res.sendStatus(401);
+    if (!result) return res.status(401).json({ message: "You don't have role access" });
     next();
   };
 };
